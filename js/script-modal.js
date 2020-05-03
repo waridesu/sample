e1fc1042e0c4a2c0004c862c5c3e567261c6e31e
Denis <waridesu@gmail.com>
2020-05-03 19:40:47 +0300

    scrolls

--- js/script-modal.js
+++ js/script-modal.js
@@ -1,14 +1,16 @@
 var o = document.querySelector(".poster-gallery")
 var mSlider = document.querySelector(".slider")
 o.addEventListener("click", function() {
+    document.body.style.overflow = "hidden";
     mSlider.classList.add('active-modal');
     document.querySelector(".burger").style.display = "none";
-    document.body.style.overflow = "hidden";
+    
 });
 
 var mClose = document.querySelector(".close-modal")
 mClose.addEventListener("click", function() {
+    document.body.style.overflow = "auto";
     mSlider.classList.remove('active-modal');
     document.querySelector(".burger").style.display = "block";
-    document.body.style.overflow = "auto";
+    
 });
